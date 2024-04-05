d378987854442d6a53bec4b6ddf52458c96e711d
muppidishivani <133558846+muppidishivani@users.noreply.github.com>
2024-04-05 07:10:19 +0530

    Add files via upload

---
+++ MilkMilliliters_3.java
@@ -0,0 +1,10 @@
+package com.web.java;
+
+public class MilkMilliliters_3 {
+	 public static void main(String[] args) {
+	        double liters = 2.5678;
+	        double milliliters = liters * 1000;
+
+	        System.out.println("There are " + milliliters + " milliliters in 2.5678 liters of milk.");
+	    }
+}
